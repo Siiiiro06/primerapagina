8edeb35d61ab2b0cc338718889b9e78c5ce4505b
Siiiiro06 <evaliullin@inscaparrella.cat>
2025-04-22 17:02:41 +0200

    ejercicio DOM
    algunos comenzados

---
+++ BF-3/E3/js/a4.js
@@ -0,0 +1,5 @@
+var pri = prompt('Escribe un numero entero positivo');
+while(pri=0){
+    pri--
+    console.log (pri)
+}
---
+++ BF-3/E7/js/a1.js
@@ -0,0 +1,2 @@
+var alumne=document.getElementById("b1");
+    alumne.style.color="#e03030"
