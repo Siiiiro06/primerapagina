546e2ce70a1c3c199e1d425f02d96cb3771618eb
Siiiiro06 <evaliullin@inscaparrella.cat>
2025-04-29 16:56:05 +0200

    Poratofolio empezadp
    Avamce del portafolio y alguna actividad

--- BF-3/E7/js/a1.js
+++ BF-3/E7/js/a1.js
@@ -1,2 +1,32 @@
-var alumne=document.getElementById("b1");
-    alumne.style.color="#e03030"
+function changeTextStyle(idelem, className) {
+    var element=document.getElementById(idelem);
+    element.style.color="#4ae04a";
+    var elems = document.getElementsByClassName(className);
+    for(var pos = 0; pos < elems.length; pos++) {
+        elems[pos].style.display = "none";
+    } 
+}
+function changeTextStyle2(idelem, className) {
+    var element=document.getElementById(idelem);
+    element.style.color="#e03030";
+    var elems = document.getElementsByClassName(className);
+    for(var pos = 0; pos < elems.length; pos++) {
+        elems[pos].style.display = "none";
+    }
+}
+
+/*function changeTextStyle('#a2') {
+    a1.style.color="#e03030"
+}  
+function changeTextStyle('#a3') {
+    a2.style.color="#e03030"
+} 
+function changeTextStyle('#a1') {
+    a2.style.color="#4ae04a"
+} 
+function changeTextStyle('#a2') {
+    a3.style.color="#4ae04a"
+} 
+function changeTextStyle('#a3') {
+    a3.style.color="#4ae04a"
+} */
