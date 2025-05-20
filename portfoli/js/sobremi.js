3225ad3187747d3c8d1c388c1bd0ed1012a70a5a
Siiiiro06 <evaliullin@inscaparrella.cat>
2025-05-20 19:52:54 +0200

    Enllazos
    Enllazos posats cporrectamente y portfoli

---
+++ portfoli/js/sobremi.js
@@ -0,0 +1,18 @@
+var images=['img/foto1.jpg','img/foto2.jpeg','img/foto3.jpg','img/foto4.jpg','img/foto5.jpg','img/foto6.jpg'];
+var counter = 0;
+function nextImage(){
+    counter++;
+    if(counter==images.length){
+        counter=0;
+    }
+    var elem=document.getElementById("carusel");
+    elem.src=images[counter];
+}
+function postImage(){
+    counter--;
+    if(counter==images.length){
+        counter=0;
+    }
+    var elem=document.getElementById("carusel");
+    elem.src=images[counter];
+}
