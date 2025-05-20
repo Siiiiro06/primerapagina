dccb998b65c18d07181aff5705f52eafbb535f7d
Siiiiro06 <evaliullin@inscaparrella.cat>
2025-05-20 16:46:07 +0200

    CSS
    acabat css y avanzat porfoli

---
+++ BF-3/E7/js/a2.js
@@ -0,0 +1,8 @@
+function calcul() {
+    var num1  = document.getElementsByClassName("num1");
+    var num2  = document.getElementsByClassName("num2");
+    var suma = (num2 + num1);
+    var pres = document.getElementsByClassName("pres")
+    pres.innerHTML = suma;
+} 
+    
