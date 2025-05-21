4ee69f6b11d1d4d1e8901ecc59ea592bb93db3cd
Siiiiro06 <evaliullin@inscaparrella.cat>
2025-05-21 17:43:23 +0200

    links corretgits
    lisk de activitats cretgides i portfoli

--- portfoli/js/sobremi.js
+++ portfoli/js/sobremi.js
@@ -1,4 +1,4 @@
-var images=['img/foto1.jpg','img/foto2.jpeg','img/foto3.jpg','img/foto4.jpg','img/foto5.jpg','img/foto6.jpg'];
+var images=['img/foto1.jpg','img/foto2.jpg','img/foto3.jpg','img/foto4.jpg','img/foto5.jpg'];
 var counter = 0;
 function nextImage(){
     counter++;
